83d2b8fb62f6b7bcaee28863a8d478341ae1128d
Woosub-Kim <woosubgo@gmail.com>
2021-08-26 22:08:37 +0900

    main func exit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'probeit',
-    version = 'v1.4',
+    version = 'v1.7',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
