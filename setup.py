62430eeb1082f361040af333fbf0a2fdfce3c07f
Woosub-Kim <woosubgo@gmail.com>
2025-04-08 14:53:19 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name = 'probeit',
     version = 'v2.1',
     packages = find_packages(),
-    entry_points = {'console_scripts': ['probeit = probeit.__main__:main']}
+    entry_points = {'console_scripts': ['probeit = src.__main__:main']}
 )
